3f7939ab42e6506fc61aa282b7d2425a24ffc8cd
Lauri Korts-Pärn <lauri@korc.jp>
2016-12-25 07:02:33 -0500

    update IP display for conky

--- home/user/.conky.lua
+++ home/user/.conky.lua
@@ -17,14 +17,17 @@ function conky_gwifinfo()
     for _,i in pairs(iface_list()) do
         local ip=conky_parse("${addrs "..i.."}")
         if ip ~= '0.0.0.0' and not (ip == '127.0.0.1' and i == 'lo') then
-            ret_str = ip
+            ret_str = ip:gsub(',','\n')
             if i == gw_iface then
-                ret_str = "${font :size=14}" .. ret_str .. "${font}\n(via " .. gw_ip .. " dev " .. i .. ")"
+                ret_str = "${font :size=14}${color green}" .. ret_str .. "${color}${font}\n(via " .. gw_ip .. " dev " .. i .. ")"
             else
                 ret_str = i .. ": " .. ret_str
             end
             ret[#ret+1] = ret_str
         end
+    end
+    if #ret == 0 then
+        return "${color red}No public IP addresses${color}"
     end
     return table.concat(ret, "\n")
 end
